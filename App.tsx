626c372ac191a35e0d03dec69e55291cabd52e37
Cory Chambers <corymchambers1@gmail.com>
2023-04-30 08:26:57 -0400

    Update App.tsx to add url polyfill and remove the auth context now handled by redux.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
 import 'react-native-gesture-handler';
+import 'react-native-url-polyfill/auto';
 
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 
-import SplashScreen from 'react-native-splash-screen';
 import {Provider} from 'react-redux';
 
 import {store} from './src/redux/store';
 
-import AuthContext from './src/auth/authContext';
 import {getIsOnboarded} from './src/auth/authStorage';
 
 import Navigator from './src/navigation/Navigator';
 
 function App(): JSX.Element {
-  const [onboarded, setOnboarded] = useState(false);
-
   useEffect(() => {
     checkOnboarded();
   }, []);
@@ -22,19 +19,12 @@ function App(): JSX.Element {
   const checkOnboarded = async () => {
     const isOnboarded = await getIsOnboarded();
     if (isOnboarded) {
-      setOnboarded(true);
     }
   };
 
-  useEffect(() => {
-    SplashScreen.hide();
-  }, []);
-
   return (
     <Provider store={store}>
-      <AuthContext.Provider value={{onboarded, setOnboarded}}>
-        <Navigator />
-      </AuthContext.Provider>
+      <Navigator />
     </Provider>
   );
 }
